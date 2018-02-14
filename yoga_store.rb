472fdfea1ba0e65dfffdbc2bcd7b9a11a8c89eba
Peter Schrammen <b.schrammen@gmail.com>
2018-02-14 19:15:25 +0100

    define 3 mehtods and a while loop for infinity shopping if wanted

--- yoga_store.rb
+++ yoga_store.rb
@@ -1,11 +1,35 @@
-shopping_card  =  []
+@shopping_card  =  []
 
 def find_product_by_reference_number(reference_number)
   @products.each { |product|
     if product[:reference_number] == reference_number
-        product 
+        @shopping_card << product
     end
   }
+end
+
+def return_the_total_price
+  total_price =  0
+  @shopping_card.each { |product|
+    total_price = total_price + product[:price]
+  }
+  puts "The total amount is: #{total_price}€"
+end
+
+def print_and_order
+  @products.each { |hashproduct|
+    puts "Ref#:#{hashproduct[:reference_number]} #{hashproduct[:name]}, it costs #{hashproduct[:price]}€"
+  }
+
+  print "Please enter the Ref# of the product you want to buy: "
+  ordernumber = gets.to_i
+
+  find_product_by_reference_number(ordernumber)
+  
+  @shopping_card.each { |product|
+    puts "You have ordered #{product[:name]} for the price of: #{product[:price]} €"
+  }    
+  return_the_total_price
 end
 
 @products = [
@@ -19,15 +43,20 @@ end
   { reference_number: 1238, name: "Light On Yoga", price: 10 }
 ]
 
-puts "Hello customer. Here are our pruducts:"
-@products.each { |hashproduct|
-  puts "#{hashproduct[:reference_number]}) #{hashproduct[:name]}, it costs #{hashproduct[:price]}€"
-}
+puts "Welcome customer, here are our pruducts:"
+print_and_order
 
-puts "please enter the number of the product you want to buy: "
-ordernumber = gets.to_i
-
-shopping_card= find_product_by_reference_number(ordernumber)
-
-puts "You have ordered #{shopping_card[0][:name]} for the price of: #{shopping_card[0][:price]} €"
-
+while true
+  print "Do you want to continue(y/n)? "
+  answer = gets.chomp.upcase
+  if answer == "N"
+    puts "Thanks for buying!"
+    return_the_total_price
+    break
+  elsif answer == "Y"
+    print_and_order
+  else
+    puts "You entered an invalid character!"
+    return_the_total_price
+  end
+end
